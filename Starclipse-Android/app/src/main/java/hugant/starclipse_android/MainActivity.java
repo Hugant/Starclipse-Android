d11b75b0e06f57e97d499be13e483f53c48a2f71
Hugant <hugantmd@yandex.ru>
2017-04-13 23:25:38 +0700

    Adds logo images

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/MainActivity.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/MainActivity.java
@@ -2,12 +2,30 @@ package hugant.starclipse_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static int i = 0;
+    Button btn;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        text = (TextView) findViewById(R.id.textView2);
+        text.setText(i + "");
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                i++;
+                text.setText(i + "");
+            }
+        });
     }
 }
