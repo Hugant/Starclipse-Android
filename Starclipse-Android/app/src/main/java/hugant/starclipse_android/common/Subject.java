0a656db27da7d3a1e9fa078d3b4aae71f081e04b
Hugant <hugantmd@yandex.ru>
2017-04-18 00:38:35 +0700

    Modified constructor

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/common/Subject.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/common/Subject.java
@@ -94,11 +94,13 @@ public class Subject {
 		} else {
 			this.value = new ScaleNumber(value);
 		}
-		
-		if (new ScaleNumber(maxValue).getPrefix().doubleValue() < 0) {
-			throw new IllegalArgumentException("The maximum amount of material cannot be less than zero");
-		} else {
-			this.maxValue = new ScaleNumber(maxValue);
+
+		if (maxValue != null) {
+			if (new ScaleNumber(maxValue).getPrefix().doubleValue() < 0) {
+				throw new IllegalArgumentException("The maximum amount of material cannot be less than zero");
+			} else {
+				this.maxValue = new ScaleNumber(maxValue);
+			}
 		}
 		
 		this.type = type == null ? type : type.toLowerCase();
