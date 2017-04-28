f04efca67c0b0dfdfcc47b0fb3d493bffd9f0a58
Hugant <hugantMD@yandex.ru>
2017-04-28 12:33:33 +0700

    Modified class

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/Travel/TravelActivity.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/Travel/TravelActivity.java
@@ -1,8 +1,17 @@
 package hugant.starclipse_android.Travel;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by Hugant on 26.04.2017.
  */
 
-public class TravelActivity {
+public class TravelActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
