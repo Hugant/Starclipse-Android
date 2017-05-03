cd86e61750c4362b17077f18e2cc4c267225f2f5
Hugant <hugantMD@yandex.ru>
2017-05-04 00:08:14 +0700

    Test

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/MainActivity.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/MainActivity.java
@@ -34,10 +34,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initTextViews();
+	    planetName = (TextView) findViewById(R.id.planetName);
+
+	    textViews.put(Subject.RESIDENTS,(TextView) findViewById(R.id.residents));
+	    textViews.put(Subject.OXYGEN,   (TextView) findViewById(R.id.oxygen));
+	    textViews.put(Subject.ENERGY,   (TextView) findViewById(R.id.energy));
+	    textViews.put(Subject.MONEY,    (TextView) findViewById(R.id.money));
+	    textViews.put(Subject.WATER,    (TextView) findViewById(R.id.water));
+	    textViews.put(Subject.STONE,    (TextView) findViewById(R.id.stone));
+	    textViews.put(Subject.GOLD,     (TextView) findViewById(R.id.gold));
+	    textViews.put(Subject.IRON,     (TextView) findViewById(R.id.iron));
+	    textViews.put(Subject.COAL,     (TextView) findViewById(R.id.coal));
+	    textViews.put(Subject.TREE,     (TextView) findViewById(R.id.tree));
+	    textViews.put(Subject.SOIL,     (TextView) findViewById(R.id.soil));
+	    textViews.put(Subject.FOOD,     (TextView) findViewById(R.id.food));
 
         infrastructureButton = (Button) findViewById(R.id.infrastructureButton);
         travelButton = (Button) findViewById(R.id.travel);
+
+	    planetName.setText("Hugant's planet");
 
         travelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,23 +78,5 @@ public class MainActivity extends AppCompatActivity {
                 textViews.get(i.getType()).setText(" 0");
             }
         }
-    }
-
-    private void initTextViews() {
-        planetName = (TextView) findViewById(R.id.planetName);
-        planetName.setText("Hugant's planet");
-
-        textViews.put(Subject.RESIDENTS,(TextView) findViewById(R.id.residents));
-        textViews.put(Subject.OXYGEN,   (TextView) findViewById(R.id.oxygen));
-        textViews.put(Subject.ENERGY,   (TextView) findViewById(R.id.energy));
-        textViews.put(Subject.MONEY,    (TextView) findViewById(R.id.money));
-        textViews.put(Subject.WATER,    (TextView) findViewById(R.id.water));
-        textViews.put(Subject.STONE,    (TextView) findViewById(R.id.stone));
-        textViews.put(Subject.GOLD,     (TextView) findViewById(R.id.gold));
-        textViews.put(Subject.IRON,     (TextView) findViewById(R.id.iron));
-        textViews.put(Subject.COAL,     (TextView) findViewById(R.id.coal));
-        textViews.put(Subject.TREE,     (TextView) findViewById(R.id.tree));
-        textViews.put(Subject.SOIL,     (TextView) findViewById(R.id.soil));
-        textViews.put(Subject.FOOD,     (TextView) findViewById(R.id.food));
     }
 }
