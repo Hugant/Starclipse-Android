0106ad83420162357df6336e21a81ebd9d8b0938
Hugant <hugantMD@yandex.ru>
2017-05-04 18:49:36 +0700

    Nothing

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/House.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/House.java
@@ -68,6 +68,7 @@ public class House extends hugant.starclipse_android.Building {
 											  	Subject.IRON,   "1" , 
 											  	Subject.WATER,  "1", 
 											  	Subject.ENERGY, "1"  ));
+
 		}
 	}
 }
