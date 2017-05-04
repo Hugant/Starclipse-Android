c918621b05c882ba0eb6bdce7c7fed9671451416
Hugant <hugantMD@yandex.ru>
2017-05-04 18:50:14 +0700

    Work on the class

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/BuildingActivity.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/BuildingActivity.java
@@ -21,9 +21,17 @@ public class BuildingActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.building);
 
+	    building = (Building) getIntent().getSerializableExtra("building");
+
 	    name = (TextView) findViewById(R.id.name);
 	    description = (TextView) findViewById(R.id.description);
 	    image = (ImageView) findViewById(R.id.imageView);
+
+	    name.setText(building.getName());
+	    description.setText(building.getDescription());
+	    image.setImageResource(building.getImage());
+
+
 
     }
 
