e90a027f61c967ad04025710282322b24d5a5c45
Hugant <hugantMD@yandex.ru>
2017-05-09 00:48:40 +0700

    Modified methods
    - constructor
    - onCreate
    - onCreateView
    - onDestroy

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/BuildingFragment.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/BuildingFragment.java
@@ -24,16 +24,101 @@ public class BuildingFragment extends Fragment {
 	private Building building;
 	private Resources resources;
 
-	public BuildingFragment(Building building) {
-		// Required empty public constructor
+	private Button claim;
+	private boolean inWork = true;
+
+	public BuildingFragment(Building building, Resources resources) {
+		this.building = building;
 	}
 
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		inWork = true;
+
+		class Updater1 extends AsyncTask<Void, Void, Void> {
+			@Override
+			protected Void doInBackground(Void... unused) {
+				while (inWork) {
+					publishProgress();
+					android.util.Log.i("Hugant", "tick");
+					SystemClock.sleep(1000);
+				}
+				return null;
+			}
+
+			@Override
+			protected void onProgressUpdate(Void... values) {
+				try {
+					claim.setText(building.getTimer());
+				} catch (UnsupportedOperationException e) {
+					claim.setText("Build");
+				}
+			}
+		}
+
+		new Updater1().execute();
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_building, container, false);
+		View view = inflater.inflate(R.layout.fragment_building, container, false);
+
+		final TextView buildingName = (TextView) view.findViewById(R.id.buildingName);
+		final ImageView image = (ImageView) view.findViewById(R.id.image);
+		final TextView description = (TextView) view.findViewById(R.id.description);
+		final TextView upgraded = (TextView) view.findViewById(R.id.upgradedContent);
+		final TextView expenses = (TextView) view.findViewById(R.id.expensesContent);
+		final Button upgrade = (Button) view.findViewById(R.id.upgradeButton);
+		claim = (Button) view.findViewById(R.id.claimButton);
+
+		buildingName.setText(building.getName());
+		image.setImageResource(building.getImage());
+		description.setText(building.getDescription());
+
+		upgrade.setEnabled(false);
+
+		try {
+			claim.setText(building.getTimer());
+		} catch (UnsupportedOperationException e) {
+			claim.setText("Build");
+		}
+
+		upgrade.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View e) {
+
+			}
+		});
+
+		claim.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View e) {
+				try {
+					if (building.getTimer().equals("Start")) {
+						building.startWork();
+					} else if (building.getTimer().equals("Claim")) {
+						resources.add(building.claim());
+					}
+				} catch (UnsupportedOperationException ex) {
+					building.build();
+				}
+
+				try {
+					claim.setText(building.getTimer());
+				} catch (UnsupportedOperationException ex) {}
+			}
+		});
+
+
+		return view;
 	}
 
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		inWork = false;
+	}
 }
