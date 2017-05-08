3363b6975821eefd9064e3ad4925bc735a1a71cb
Hugant <hugantMD@yandex.ru>
2017-05-09 00:54:56 +0700

    Modified imports
    - support.v4.app.Fragment

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/planet/PlanetFragment.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/planet/PlanetFragment.java
@@ -2,7 +2,7 @@ package hugant.starclipse_android.planet;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
