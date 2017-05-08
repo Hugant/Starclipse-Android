9c9d8374bbea29e767fbab98ebeeed3019f3774e
Hugant <hugantMD@yandex.ru>
2017-05-08 11:48:18 +0700

    Added imports
    - ListView
    - ArrayList
    - Planet

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/travel/TravelFragment.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/travel/TravelFragment.java
@@ -6,8 +6,12 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import hugant.starclipse_android.R;
+import hugant.starclipse_android.planet.Planet;
 
 
 /**
