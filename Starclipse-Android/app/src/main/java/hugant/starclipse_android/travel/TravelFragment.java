00a49841d45f10201c64394e0f6c3791fa424dcb
Hugant <hugantMD@yandex.ru>
2017-05-08 11:49:40 +0700

    Modified method onCreateView

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/travel/TravelFragment.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/travel/TravelFragment.java
@@ -30,8 +30,14 @@ public class TravelFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_travel, container, false);
+
+		View view = inflater.inflate(R.layout.fragment_travel, container, false);
+
+		adapter = new TravelAdapter(getActivity(), planets);
+		ListView listView = (ListView) view.findViewById(R.id.travelAdapter);
+		listView.setAdapter(adapter);
+
+		return view;
 	}
 
 }
