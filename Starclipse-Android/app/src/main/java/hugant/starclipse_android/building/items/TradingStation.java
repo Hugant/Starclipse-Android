65f736768fb08b50138d4bad4a7ab8d13dc1d612
Hugant <hugantMD@yandex.ru>
2017-05-15 21:15:07 +0700

    Modified class

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/items/TradingStation.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/items/TradingStation.java
@@ -1,5 +1,17 @@
 package hugant.starclipse_android.building.items;
 
-public class TradingStation {
+import hugant.starclipse_android.R;
+import hugant.starclipse_android.building.Building;
+import hugant.starclipse_android.common.Resources;
+
+public class TradingStation extends Building {
+
+	public TradingStation() {
+		super.setName(R.string.building_trading_station_name);
+		super.setDescription(R.string.building_trading_station_description);
+		super.setImage(R.drawable.building_trade_station_150dp);
+		super.setExpenses(new Resources());
+		super.setResidents("0", "10");
+	}
 
 }
