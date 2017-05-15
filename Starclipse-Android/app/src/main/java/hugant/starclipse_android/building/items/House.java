fbfa8d5f6bb5ea4f0d8a6d611ce7f1b27d6a0c20
Hugant <hugantMD@yandex.ru>
2017-05-15 21:14:23 +0700

    Modified constructor

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/items/House.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/items/House.java
@@ -2,9 +2,9 @@ package hugant.starclipse_android.building.items;
 
 import hugant.starclipse_android.R;
 import hugant.starclipse_android.building.Building;
-import hugant.starclipse_android.common.Timer;
-import hugant.starclipse_android.common.Subject;
 import hugant.starclipse_android.common.Resources;
+import hugant.starclipse_android.common.Subject;
+import hugant.starclipse_android.common.Timer;
 
 /**
  * This class implements an object <b>House</b> that has three types:
@@ -28,10 +28,11 @@ public class House extends Building {
 		switch (type.toLowerCase()) {
 			case "big":
 				super.setResidents("0", "100");
-				super.setName("Big House");
+				super.setName(R.string.building_big_house_name);
 				super.setBuildTimer(Timer.MINUTE * 30, "Start");
 				super.setIncomeTimer(Timer.SECOND * 20, "Claim");
-//				super.setDescription(R.string.building_small_house_description);
+				super.setDescription(R.string.building_big_house_description);
+				super.setImage(R.drawable.building_big_house_150dp);
 				super.setIncome(new Resources(Subject.RESIDENTS, "10", Subject.MONEY, "100"));
 				super.setExpenses(new Resources(Subject.STONE,  "100",
 											  	Subject.TREE,   "50", 
@@ -43,10 +44,11 @@ public class House extends Building {
 				
 			case "average":
 				super.setResidents("0", "50");
-				super.setName("Average House");
+				super.setName(R.string.building_average_house_name);
 				super.setBuildTimer(Timer.MINUTE * 5, "Start");
 				super.setIncomeTimer(Timer.SECOND * 20, "Claim");
-//				super.setDescription(R.string.building_small_house_description);
+				super.setDescription(R.string.building_average_house_description);
+				super.setImage(R.drawable.building_average_house_150dp);
 				super.setIncome(new Resources(Subject.RESIDENTS, "5", Subject.MONEY, "50"));
 				super.setExpenses(new Resources(Subject.STONE,  "50", 
 											  	Subject.TREE,   "25", 
@@ -58,10 +60,11 @@ public class House extends Building {
 				
 			case "small":
 				super.setResidents("0", "10");
-				super.setName("Small House");
+				super.setName(R.string.building_small_house_name);
 				super.setBuildTimer(Timer.SECOND * 10, "Start");
 				super.setIncomeTimer(Timer.SECOND * 5, "Claim");
 				super.setDescription(R.string.building_small_house_description);
+				super.setImage(R.drawable.building_small_house_150dp);
 				super.setIncome(new Resources(Subject.RESIDENTS, "500", Subject.MONEY, "500"));
 				super.setExpenses(new Resources(Subject.STONE,  "12", 
 												Subject.TREE,   "6", 
