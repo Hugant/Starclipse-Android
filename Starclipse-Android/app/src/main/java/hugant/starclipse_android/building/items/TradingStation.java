19886ce4461a2ee5f1c89494a0f82842e9102c8d
Hugant <hugantMD@yandex.ru>
2017-05-16 00:15:46 +0700

    Modifeid class

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/items/TradingStation.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/items/TradingStation.java
@@ -6,7 +6,14 @@ import hugant.starclipse_android.common.Resources;
 
 public class TradingStation extends Building {
 
-	public TradingStation() {
+	private Resources resources;
+	private Resources offer;
+	private Resources price;
+	private int amountOfTrades = 0;
+	private int status = 0;
+
+	public TradingStation(Resources resources) {
+		this.resources = resources;
 		super.setName(R.string.building_trading_station_name);
 		super.setDescription(R.string.building_trading_station_description);
 		super.setImage(R.drawable.building_trade_station_150dp);
@@ -14,4 +21,45 @@ public class TradingStation extends Building {
 		super.setResidents("0", "10");
 	}
 
+	public void setOffer(Resources offer) {
+		this.offer = offer;
+	}
+
+	public Resources getOffer() {
+		return offer;
+	}
+
+	public void setPrice(Resources price) {
+		this.price = price;
+	}
+
+	public Resources getPrice() {
+		return price;
+	}
+
+	public void trade() {
+		amountOfTrades++;
+	}
+
+	public boolean canTrade() {
+		// if resources - offer < 0
+		return false;
+	}
+
+	@Override
+	public Resources claim() {
+//		resources.add(offer * amountOFTrades);
+		return null;
+	}
+
+	public int getStatus() {
+		if (status == -1) {
+			//return build
+		} else if (status == 0) {
+			// return no trades
+		} else if (status > 0) {
+			//return number of trades
+		}
+		return 0;
+	}
 }
