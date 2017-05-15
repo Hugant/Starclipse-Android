a7ebfb65677d4154f1bd6dee516f341fde5bd4d8
Hugant <hugantMD@yandex.ru>
2017-05-15 21:14:58 +0700

    Modified constructor

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/items/industry/StarshipsFactory.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/items/industry/StarshipsFactory.java
@@ -1,20 +1,22 @@
 package hugant.starclipse_android.building.items.industry;
 
+import hugant.starclipse_android.R;
 import hugant.starclipse_android.building.Building;
 import hugant.starclipse_android.common.Timer;
 import hugant.starclipse_android.common.Resources;
 
 public class StarshipsFactory extends Building {
-	private String name = "Starships Factory";
 	private Starship starship = null;
 	private int manpower = 0;
 	private Resources expenses = null;
 			
 	public StarshipsFactory() {
-		super.setName("Starships Factory");
+		super.setName(R.string.building_starships_factory_name);
+		super.setDescription(R.string.building_starships_factory_description);
+		super.setImage(R.drawable.building_starship_factory_150dp);
 		super.setBuildTimer(30 * Timer.DAY, "Start");
 		super.setIncomeTimer(10 * Timer.DAY, "Claim");
-		super.setResidents("0", "");
+		super.setResidents("0", "10");
 		super.setExpenses(new Resources());
 	}
 	
@@ -22,9 +24,5 @@ public class StarshipsFactory extends Building {
 	
 	public Starship getStarship() {
 		return this.starship;
-	}
-	
-	public String getName() {
-		return this.name;
 	}
 }
