6569a1b55177d4ec3dcf13c7f2c9a2c4e434742d
Hugant <hugantMD@yandex.ru>
2017-05-16 22:56:23 +0700

    Modified class

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/travel/TravelAdapter.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/travel/TravelAdapter.java
@@ -1,31 +1,32 @@
 package hugant.starclipse_android.travel;
 
-import android.app.Activity;
-import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
+import android.content.Context;
 import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View;
 
 import java.util.ArrayList;
 
+import hugant.starclipse_android.planet.Planet;
 import hugant.starclipse_android.MainActivity;
 import hugant.starclipse_android.R;
-import hugant.starclipse_android.planet.Planet;
 
 /**
+ * TODO: write javadoc
+ *
  * Created by Hugant on 07.05.2017.
  */
 
-public class TravelAdapter extends BaseAdapter {
+class TravelAdapter extends BaseAdapter {
 
 	private Context context;
 	private ArrayList<Planet> planets;
 	private LayoutInflater layoutInflater;
 
-	public TravelAdapter(Context context, ArrayList<Planet> planets) {
+	TravelAdapter(Context context, ArrayList<Planet> planets) {
 		this.context = context;
 		this.planets = planets;
 		layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
