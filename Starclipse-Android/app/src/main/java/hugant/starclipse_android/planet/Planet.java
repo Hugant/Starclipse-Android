68dd43c3beb165bd389f75da770548ca8be5b757
Hugant <hugantMD@yandex.ru>
2017-05-18 21:12:18 +0700

    Added planets massive

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/planet/Planet.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/planet/Planet.java
@@ -5,6 +5,36 @@ import hugant.starclipse_android.common.Resources;
 import hugant.starclipse_android.common.Subject;
 
 public class Planet {
+	public static final Planet[] PLANETS = {
+			new Planet("Caroline",  new Resources(), R.drawable.planet1, 0),
+			new Planet("Amine",     new Resources(), R.drawable.planet2, 0),
+			new Planet("Kate",      new Resources(), R.drawable.planet3, 0),
+			new Planet("Emily",     new Resources(), R.drawable.planet4, 0),
+			new Planet("Hannah",    new Resources(), R.drawable.planet5, 0),
+			new Planet("Brianna",   new Resources(), R.drawable.planet6, 0),
+			new Planet("Hailey",    new Resources(), R.drawable.planet7, 0),
+			new Planet("Kaylee",    new Resources(), R.drawable.planet8, 0),
+			new Planet("Taylor",    new Resources(), R.drawable.planet9, 0),
+			new Planet("Ashley",    new Resources(), R.drawable.planet10, 0),
+			new Planet("Lauren",    new Resources(), R.drawable.planet11, 0),
+			new Planet("Kayla",     new Resources(), R.drawable.planet12, 0),
+			new Planet("Madeline",  new Resources(), R.drawable.planet13, 0),
+			new Planet("Matthew",   new Resources(), R.drawable.planet14, 0),
+			new Planet("Joseph",    new Resources(), R.drawable.planet15, 0),
+			new Planet("Joshua",    new Resources(), R.drawable.planet16, 0),
+			new Planet("Andrew",    new Resources(), R.drawable.planet17, 0),
+			new Planet("Ryan",      new Resources(), R.drawable.planet18, 0),
+			new Planet("Kayla",     new Resources(), R.drawable.planet19, 0),
+			new Planet("Hoggarth",  new Resources(), R.drawable.planet20, 0),
+			new Planet("Allford",   new Resources(), R.drawable.planet21, 0),
+			new Planet("Baldwin",   new Resources(), R.drawable.planet22, 0),
+			new Planet("Barnes",    new Resources(), R.drawable.planet23, 0),
+			new Planet("Carroll",   new Resources(), R.drawable.planet24, 0),
+			new Planet("Carrington",new Resources(), R.drawable.planet25, 0),
+			new Planet("Derrik",    new Resources(), R.drawable.planet26, 0),
+			new Planet("Hailey",    new Resources(), R.drawable.planet27, 0),
+	};
+
 	private Resources resources;
 	private Resources effect;
 	
