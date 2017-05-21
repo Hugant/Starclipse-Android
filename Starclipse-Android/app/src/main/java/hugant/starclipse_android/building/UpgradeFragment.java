68d27eb7aca325e18c1627149a9b9adca46ed41b
Hugant <hugantMD@yandex.ru>
2017-05-21 16:00:47 +0700

    Created class

---
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/UpgradeFragment.java
@@ -0,0 +1,39 @@
+package hugant.starclipse_android.building;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import hugant.starclipse_android.R;
+import hugant.starclipse_android.common.Resources;
+
+/**
+ *
+ * Created by Hugant on 20.05.2017.
+ */
+
+public class UpgradeFragment extends Fragment {
+
+	private Building building;
+	private Resources resources;
+
+	public UpgradeFragment(Building building, Resources resources) {
+		this.building = building;
+		this.resources = resources;
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+	                         @Nullable Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_building_upgrade, container, false);
+
+		((TextView) view.findViewById(R.id.buildingName)).setText(building.getName());
+		return view;
+	}
+}
