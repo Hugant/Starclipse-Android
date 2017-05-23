98aeb95ea5e84616a4e0935f43b5247aa7befb98
Hugant <hugantMD@yandex.ru>
2017-05-24 00:54:58 +0700

    View for fragment_building_upgrade

--- Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/UpgradeFragment.java
+++ Starclipse-Android/app/src/main/java/hugant/starclipse_android/building/UpgradeFragment.java
@@ -32,12 +32,90 @@ public class UpgradeFragment extends Fragment {
 
 	@Nullable
 	@Override
-	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+	public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container,
 	                         @Nullable Bundle savedInstanceState) {
 
 		View view = inflater.inflate(R.layout.fragment_building_upgrade, container, false);
 
+		final LinearLayout currentResourcesList =
+				(LinearLayout) view.findViewById(R.id.currentResourcesList);
+		final LinearLayout upgradedResourcesList =
+				(LinearLayout) view.findViewById(R.id.upgradedResourcesList);
+		final LinearLayout expensesResourcesList =
+				(LinearLayout) view.findViewById(R.id.expensesResourcesList);
+		final Button upgrade = (Button) view.findViewById(R.id.upgradeButton);
+
 		((TextView) view.findViewById(R.id.buildingName)).setText(building.getName());
+
+		Building.fillList(getContext(), currentResourcesList,
+				R.layout.resources_item_minimal, building, "income");
+
+		Building.fillList(getContext(), upgradedResourcesList,
+				R.layout.resources_item_minimal, building, "upgraded income");
+
+		Building.fillList(getContext(), expensesResourcesList,
+				R.layout.resources_item_minimal, building, "expenses");
+
+		if (!resources.canSubtract(building.getExpenses())) {
+			upgrade.setEnabled(false);
+		}
+
+			upgrade.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (resources.canSubtract(building.getExpenses())) {
+					building.upgrade();
+
+					Building.fillList(getContext(), currentResourcesList,
+							R.layout.resources_item_minimal, building, "income");
+
+					Building.fillList(getContext(), upgradedResourcesList,
+							R.layout.resources_item_minimal, building, "upgraded income");
+
+					Building.fillList(getContext(), expensesResourcesList,
+							R.layout.resources_item_minimal, building, "expenses");
+				} else {
+					upgrade.setEnabled(false);
+				}
+			}
+		});
+
+
+//		for (Subject i : cloneBuilding
+//				.getIncome().multiply(building.getResidents().getValue()).getSubjects()) {
+//			View linearChild = inflater.inflate(R.layout.resources_item_minimal, currentResourcesList, false);
+//
+//			((ImageView) linearChild.findViewById(R.id.resourceIcon)).setImageResource(i.getImage());
+//			((TextView) linearChild.findViewById(R.id.values))
+//					.setText(i.getValue() + (i.getMaxValue() == null ? "" : " / " + i.getMaxValue()));
+//
+//			currentResourcesList.addView(linearChild);
+//		}
+
+
+//		for (Subject i : cloneBuilding.upgrade()
+//				.getIncome().multiply(building.getResidents().getValue()).getSubjects()) {
+//			View linearChild = inflater.inflate(R.layout.resources_item_minimal, upgradedResourcesList, false);
+//
+//			((ImageView) linearChild.findViewById(R.id.resourceIcon)).setImageResource(i.getImage());
+//			((TextView) linearChild.findViewById(R.id.values))
+//					.setText(i.getValue() + (i.getMaxValue() == null ? "" : " / " + i.getMaxValue()));
+//
+//			upgradedResourcesList.addView(linearChild);
+//		}
+
+//		for (Subject i : cloneBuilding.getExpenses().getSubjects()) {
+//			View linearChild = inflater.inflate(R.layout.resources_item, expensesResourcesList, false);
+//
+//			((ImageView) linearChild.findViewById(R.id.resourceIcon)).setImageResource(i.getImage());
+//			((TextView) linearChild.findViewById(R.id.resourceName)).setText(i.getType());
+//			((TextView) linearChild.findViewById(R.id.values))
+//					.setText(i.getValue() + (i.getMaxValue() == null ? "" : " / " + i.getMaxValue()));
+//
+//			expensesResourcesList.addView(linearChild);
+//		}
+
+
 		return view;
 	}
 }
